1eedf6a6a0496a159ae4df142d33add4d49a38f3
muzair321 <69965044+muzair321@users.noreply.github.com>
2025-10-08 19:56:47 +0500

    Create playing_with_char.c

---
+++ playing_with_char.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    char ch;
+    scanf("%c", &ch); 
+    printf("%c\n", ch);
+    char s[100];
+    scanf("%s", &s); 
+    printf("%s\n", s);
+    char sen[100];
+    scanf(" %[^\n]", &sen); 
+    printf("%s\n",sen);    
+    return 0;
+}
